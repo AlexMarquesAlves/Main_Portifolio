39252ff50c7728d6664d4fe708d60124ed1f93ba
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-05 14:59:35 -0300

    Finalizando a sessão do Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,44 @@
+import {
+  AiOutlineTwitter,
+  AiOutlineGithub,
+  AiFillLinkedin
+} from 'react-icons/ai';
 import { Container } from './styles';
 
 function Footer() {
+  function handleRedirect(url: string) {
+    window.open(url);
+  }
+
+  function handleScrollTop() {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
   return (
     <Container>
-      <h1>Footer</h1>
+      <div className="container">
+        <section>
+          <AiOutlineTwitter
+            onClick={() => handleRedirect('https://twitter.com/meArchi_Duque')}
+          />
+          <AiOutlineGithub
+            onClick={() =>
+              handleRedirect('https://github.com/AlexMarquesAlves')
+            }
+          />
+          <AiFillLinkedin
+            onClick={() =>
+              handleRedirect('https://www.linkedin.com/in/alexmalves/')
+            }
+          />
+        </section>
+        <button type="button" onClick={handleScrollTop}>
+          Back to top
+        </button>
+      </div>
     </Container>
   );
 }
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,5 +1,61 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.footer`
+  ${({ theme }) => css`
+    width: 100%;
+    display: flex;
+    margin-top: 10rem;
+    border-top: 1px solid ${theme.backgroundLight};
+    height: 5rem;
+    align-items: center;
+    justify-content: center;
 
+    > div {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      button {
+        background: none;
+        border: none;
+        color: ${theme.secondary};
+        text-transform: uppercase;
+        font-weight: 300;
+        font-size: 1.2rem;
+      }
+
+      > section {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+
+        svg {
+          width: 2rem;
+          height: 2rem;
+          color: ${theme.text};
+          transition: 0.5s;
+          cursor: pointer;
+
+          &:hover {
+            color: ${theme.primary};
+          }
+        }
+      }
+
+      @media (max-width: 450px) {
+        button {
+          font-size: 0.9rem;
+        }
+
+        > section {
+          gap: 0.5rem;
+
+          svg {
+            width: 1.5rem;
+            height: 1.5rem;
+          }
+        }
+      }
+    }
+  `}
 `;
