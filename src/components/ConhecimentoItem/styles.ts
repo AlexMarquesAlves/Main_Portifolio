f194bb590a1d32c1576e57d9f94851b722ec08f7
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-05 13:37:34 -0300

    Finalizando a sessão de Conhecimentos

--- src/components/ConhecimentoItem/styles.ts
+++ src/components/ConhecimentoItem/styles.ts
@@ -1,5 +1,43 @@
 import styled, { css } from 'styled-components';
 
 export const ConhecimentoContainer = styled.div`
-  ${({ theme }) => css``}
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    gap: 0.8rem;
+    align-items: center;
+    justify-content: center;
+
+    p {
+      color: ${theme.primary};
+      font-weight: 300;
+      font-size: 1.3rem;
+      text-transform: uppercase;
+    }
+
+    svg {
+      width: 6.5rem;
+      height: 6.5rem;
+      color: ${theme.secondary};
+      transition: 0.3s;
+    }
+
+    &:hover {
+      svg {
+        color: ${theme.primary};
+        transform: scale(0.95);
+      }
+    }
+
+    @media (max-width: 1000px) {
+      p {
+        font-size: 1rem;
+      }
+
+      svg {
+        width: 5rem;
+        height: 5rem;
+      }
+    }
+  `}
 `;
