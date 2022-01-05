eee074e737c69ce4c2f02acbe12108d276acc30b
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-05 09:03:30 -0300

    Criando Componente Experiencias

---
+++ src/components/Experiencias/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+function Experiencias() {
+  return (
+    <Container>
+      <h1>Experiencias</h1>
+    </Container>
+  );
+}
+
+export default Experiencias;
