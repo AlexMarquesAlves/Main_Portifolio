8e94cd40536a55d3d5970b270e5efd48afa8288f
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-05 09:00:34 -0300

    Componente HomeHero da Página

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,8 @@
 const theme = {
   background: '#11172B',
   backgroundLight: '#1B2138',
-  primary: '#0EE7B7',
-  secondary: '#7AC7E3',
+  primary: '#9400d3',
+  secondary: '#d02090',
   gradient: 'linear-gradient(225deg, #313860 0%, #11172B 100%);',
   text: '#848DA0',
   textHighlight: '#444B5B',
@@ -13,5 +13,14 @@ const theme = {
 
 export default theme;
 
+// THEMAS PADRÁO...
 // background: '#9400d3',
 // backgroundLight: '#d02090',
+// primary: '#0EE7B7',
+// secondary: '#7AC7E3',
+// gradient: 'linear-gradient(225deg, #313860 0%, #11172B 100%);',
+// text: '#848DA0',
+// textHighlight: '#444B5B',
+// textLight: '#C4C4C4',
+// border: '#313958',
+// inputBackground: '#1E253E'
