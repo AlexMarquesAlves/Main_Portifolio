ac27baf0331c3b0fbf0c99885af231d4e6b6d2d4
¡Duque <atanaelleonardo@gmail.com>
2023-01-30 22:39:33 -0300

    update show

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import { Container, TextContainer, InfosContainer, CodeItem } from './styles';
 import picture from '../../assets/profile.png';
+import { CodeItem, Container, InfosContainer, TextContainer } from './styles';
 
 function HomeHero() {
   return (
@@ -9,14 +9,14 @@ function HomeHero() {
       <div>
         <TextContainer>
           <h1>Olá</h1>
-          <h2>Chamo-me Duque</h2>
+          <h2>Chamo-me ¡Duque</h2>
         </TextContainer>
         <InfosContainer>
           <CodeItem data-aos="zoom-in">
             <span className="comment">//Minha apresentação</span>
             <span className="purple">Infos</span> {'\u007B'}
             <div>
-              Nome: <span className="blue">Atanael Leonardo,</span>
+              Nome: <span className="blue">A. Leonardo,</span>
             </div>
             <div>
               Sobrenome: <span className="blue">Xavier Marques Alves</span>
@@ -26,7 +26,7 @@ function HomeHero() {
           <CodeItem data-aos="zoom-in">
             <span className="purple">Cargo</span> {'\u007B'}
             <div>
-              Função: <span className="blue">Dev Front-end,</span>
+              Função: <span className="blue">Dev Fullstack,</span>
             </div>
             <div>
               Empresa: <span className="blue">...</span>
